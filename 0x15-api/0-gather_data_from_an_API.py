36f9af2771f22ca9e9030b440810e6ba52e88f05
Aisosa <allysonaghedo@gmail.com>
2022-11-14 21:39:16 +0100

    API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress."""
+""" a Python script that, using this REST API, for a given employee
+ID, returns information about his/her TODO list progress."""
 
 if __name__ == "__main__":
     import requests
@@ -8,9 +8,9 @@ if __name__ == "__main__":
 
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(argv[1])).json()
-    todo_list = requests.get(url + "todo_list", params={"userId": argv[1]}).json()
+    todo_li = requests.get(url + "todos", params={"userId": argv[1]}).json()
 
-    completed = [t.get("title") for t in todo_list if t.get("completed") is True]
+    completed = [t.get("title") for t in todo_li if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todo_list)))
+        user.get("name"), len(completed), len(todo_li)))
     [print("\t {}".format(c)) for c in completed]
