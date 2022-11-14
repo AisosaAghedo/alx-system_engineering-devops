82070c013459efbfd0eae1da34c6892b696f4a2c
Aisosa <allysonaghedo@gmail.com>
2022-11-14 22:10:18 +0100

    API

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Exporting data in  JSON format."""
+
+if __name__ == "__main__":
+    import json
+    import requests
+    from sys import argv
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
