72a1d79e971c894de4e1fef16dbce1d5d730a386
Aisosa <allysonaghedo@gmail.com>
2022-11-29 23:04:03 +0100

    api

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,7 +4,8 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after="", count=0):
-    """returns a list containing the titles of all hot articles for a given subreddit"""
+    """returns a list containing the titles of all hot
+    articles for a given subreddit"""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
