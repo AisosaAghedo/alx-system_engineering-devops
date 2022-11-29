55d931c80a350663df0875b2e57d60b7929104a9
Aisosa <allysonaghedo@gmail.com>
2022-11-29 22:23:32 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,12 +6,11 @@ import requests
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers (not active users,
     total subscribers) for a given subreddit"""
-    headers = {"User-Agent": 'Mozilla/5.0 (platform; rv:geckoversion)\
-            Gecko/geckotrail Firefox/firefoxversion'}
-    r = requests.get('https://www.reddit.com/r/programming/about.json'.
-                     format(subreddit), headers=headers,
-                     allow_redirects=False)
-    if r.status_code != 200:
+
+    headers = {"User-Agent": 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 404:
         return (0)
     else:
         return (r.json().get("data").get("subscribers"))
