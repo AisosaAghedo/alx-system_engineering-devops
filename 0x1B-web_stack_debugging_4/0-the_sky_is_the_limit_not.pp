3994ef1832d480920dd09b1123321f1455229146
Aisosa <allysonaghedo@gmail.com>
2022-12-08 21:08:45 +0100

    webstack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,5 @@
-# In this web stack debugging task, we are testing how well our web server setup featuring Nginx is doing under pressure and it turns out it’s not doing well: we are getting a huge amount of failed requests
+# In this web stack debugging task, we are testing how well our web server setup featuring Nginx is doing under pressure
+# and it turns out it’s not doing well: we are getting a huge amount of failed requests
 
 exec {'replace':
   provider => shell,
